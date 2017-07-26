95ccd751e2ba74643d4c4d61c6ef1099ff398b72
Administrator <751957846@qq.com>
2017-07-26 23:03:34 +0800

    Initial commit

---
+++ src/main/java/com/wwh/iot/easylinker/EasylinkerApplication.java
@@ -0,0 +1,12 @@
+package com.wwh.iot.easylinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasylinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasylinkerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wwh/iot/easylinker/EasylinkerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wwh.iot.easylinker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasylinkerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
