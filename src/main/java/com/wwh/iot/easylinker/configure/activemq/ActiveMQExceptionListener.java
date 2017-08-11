e7b0a3d85af12bdafa9458bd8546db6bdab42aa5
xiaoCong2014 <1149023208@qq.com>
2017-08-11 21:24:21 +0800

    创建 MQ 异常监听器

---
+++ src/main/java/com/wwh/iot/easylinker/configure/activemq/ActiveMQExceptionListener.java
@@ -0,0 +1,21 @@
+package com.wwh.iot.easylinker.configure.activemq;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+
+/**
+ * MQ 异常监听器 <br>
+ * Created by Lai Conghe on 2017-08-11
+ */
+public class ActiveMQExceptionListener implements ExceptionListener {
+
+    Logger logger = LoggerFactory.getLogger( ActiveMQExceptionListener.class );
+
+    @Override
+    public void onException( JMSException e ) {
+        logger.info("ActiveMQExceptionListener onException :" + e );
+    }
+}
